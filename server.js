389cc1dc479e4a2ff2ab9fc22538cfe7297622ae
Tayfun Sur <tayfunsur@gmail.com>
2020-06-11 07:45:40 +0300

    Create backend

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config({path: './config/config.env'});
+const colors = require('colors');
+const morgan = require('morgan');
+
+const app = express();
+
+const transactionRouter = require('./routes/transactions')
+
+app.use('/api/v1/transactions', transactionRouter)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgBlue))
